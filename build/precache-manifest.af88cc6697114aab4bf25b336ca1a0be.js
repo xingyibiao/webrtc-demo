7f94d28d9467be47d1df26280d8f7d76db909aa8
yibiao.xing <yibiao.xing@zhangmen.com>
2019-11-04 11:30:27 +0800

    feat: 增加call逻辑

---
+++ build/precache-manifest.af88cc6697114aab4bf25b336ca1a0be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4f6bfee79125f8cc1238fcc76f3267",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "68036950c953f920ec7a",
+    "url": "/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "36b8da416d04bdbf04db",
+    "url": "/build/static/js/2.8e6a1544.chunk.js"
+  },
+  {
+    "revision": "68036950c953f920ec7a",
+    "url": "/build/static/js/main.0ab76f75.chunk.js"
+  },
+  {
+    "revision": "da64493626428444ca0e",
+    "url": "/build/static/js/runtime-main.02557a4c.js"
+  }
+]);
